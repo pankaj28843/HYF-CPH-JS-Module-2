47f11e9d5b0c318efe9679a96f8d8f13e8e04e1c
Pankaj Singh <pankaj28843@gmail.com>
2017-11-05 14:10:14 +0100

    add exercise for user input

---
+++ week-4-user-input-and-structuring-code-files/exercise/user-input.js
@@ -0,0 +1,10 @@
+/**
+ * 1. Add an input element of type text so that user can enter their name
+ * 2. Add a function to get value from the input element
+ * 3. Add a button element
+ * 4. When this button is clicked it should bring an alert dialog on screen with "Hello! " followed by name entered in the input
+ *
+ *
+ */
+
+console.log('Awesome! your script has been loaded!');
